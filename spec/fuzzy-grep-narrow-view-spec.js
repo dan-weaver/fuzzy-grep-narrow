ab2ddfe0be64c06811e6ee9f9f518010bcc3c50c
Daniel Weaver <danweaver06@gmail.com>
2017-06-21 20:18:57 -0400

    first commit

---
+++ spec/fuzzy-grep-narrow-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FuzzyGrepNarrowView from '../lib/fuzzy-grep-narrow-view';
+
+describe('FuzzyGrepNarrowView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
